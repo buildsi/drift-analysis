ab210cb474f0b885bef939d52a25d25daff55491
Alec Scott <hi@alecbcs.com>
2022-04-15 13:03:12 -0700

    Update entrypoint with new expected environment vars

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,14 +1,19 @@
 #! /bin/bash
 
-if [ "$DRIFTSERVER_ACCESS_KEY_ID" != "" ]; then
+if [ "$DRIFTSERVER_S3_ACCESSKEY" != "" ]; then
     echo "dbs:" > /etc/litestream.yml
-    echo "  - path: /app/points.db" >> /etc/litestream.yml
+    echo "  - path: /app/data/points.db" >> /etc/litestream.yml
     echo "    replicas:" >> /etc/litestream.yml
-    echo "      - url: $DRIFTSERVER_REPLICA_URL" >> /etc/litestream.yml
+    echo "      - type: s3" >> /etc/litestream.yml
+    echo "        bucket: $DRIFTSERVER_S3_BUCKET" >> /etc/litestream.yml
+    echo "        access-key-id: $DRIFTSERVER_S3_ACCESSKEY" >> /etc/litestream.yml
+    echo "        secret-access-key: $DRIFTSERVER_S3_SECRETKEY" >> /etc/litestream.yml
+    echo "        path: database/points.db" >> /etc/litestream.yml
+    echo "        region: $DRIFTSERVER_S3_REGION" >> /etc/litestream.yml
     echo "        retention: 24h" >> /etc/litestream.yml
     echo "        sync-interval: 15m" >> /etc/litestream.yml
-    export LITESTREAM_SECRET_ACCESS_KEY=$DRIFTSERVER_SECRET_ACCESS_KEY
-    litestream restore -if-replica-exists -v -o points.db "$DRIFTSERVER_REPLICA_URL"
+    
+    litestream restore -if-replica-exists -v /app/data/points.db 
     litestream replicate -exec /app/drift-server
 else 
     /app/drift-server
